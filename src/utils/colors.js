18182235600c8815955e8262c8c6752625384416
mati.lechu <matiaslechuga67@gmail.com>
2023-06-14 17:30:33 -0300

    first location list

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#e63946",
+  secondary: "#329F5B",
+  tertiary: "#0D5D56",
+  background: "#E0F5E8",
+  text: "#212121",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#808080",
+};
